8c95850f4ea0fc48c7ce7e9e58e1ba5062ff4940
Ashley <ashley.tucker@yale.edu>
2015-10-10 00:22:07 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! Yes.');
+});
+app.get('/nickname', function(request, response){
+    response.send('cautious-hedgehog');
 });
 
 app.listen(process.env.PORT || 4000);
